52f87bc72cbec1a7907860b938fa5628dca867dd
narenkarthikraj <153854521+narenkarthikraj@users.noreply.github.com>
2023-12-17 00:41:04 +0530

    Add files via upload

---
+++ New folder (4)/script.js
@@ -0,0 +1,5 @@
+
+    //function that evaluate the value and returns result 
+    function kiloweightConvert(value) { 
+        document.getElementById("Maintanance Calories").innerHTML=((value*13.397)+88.362)+1.55; 
+    }
